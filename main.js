10312c68cdf07fcaf1730b16c09c2270b8035281
JLB246 <65634399+JLB246@users.noreply.github.com>
2020-07-29 15:35:03 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var answer1 = prompt('What is one of your favorite movies?');
+var answer2 = prompt('What is one of your favorite movies?');
+var answer3 = prompt('What is one of your favorite movies?');
+var answer4 = prompt('What is one of your favorite movies?');
+
+var favoriteMovies = [(answer1), (answer2), (answer3), (answer4) ]
+  
+
+for (var i = 0; i <= favoriteMovies.length; i=i+1){
+  console.log( favoriteMovies[i] + ' is one of your favorite movies');
+} 
